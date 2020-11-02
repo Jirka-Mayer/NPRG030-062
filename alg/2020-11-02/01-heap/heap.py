0e091c6556142923607acabba97201e5bd4de512
Jirka-Mayer <jirkamayer97@gmail.com>
2020-11-02 13:34:49 +0100

    API Haldy

---
+++ alg/2020-11-02/01-heap/heap.py
@@ -0,0 +1,13 @@
+
+class Heap():
+    def __init__(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def add(self, item):
+        pass
+
+    def pop(self):
+        pass
--- alg/2020-11-02/01-heap/main.py
+++ alg/2020-11-02/01-heap/main.py
@@ -1,3 +1,10 @@
-import heap
+from heap import Heap
 
-# ...
+h = Heap()
+
+data = [5, 3, 8, 4, 3, 6, 9, 5]
+for i in data:
+    h.add(i)
+
+while h.is_empty():
+    print(h.pop())
