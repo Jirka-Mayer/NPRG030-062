781fece50bcfba5c4c6849b825267b521ff9a804
Jirka-Mayer <jirkamayer97@gmail.com>
2020-11-02 15:51:40 +0100

    Záznam z 2. listopadu

--- prg/2020-11-02/01-pgm.py
+++ prg/2020-11-02/01-pgm.py
@@ -10,19 +10,7 @@ Viz. soubor "feep.pgm":
 Vyrobte si třídu PgmImage, která bude obrázek reprezentovat
 """
 
-pixels = [[0, 0.25, 0.5], [0, 0.1, 0]]
-
-class PgmImage():
-    def __init__(self, file_name):
-        self.__pixels = []
-        pass
-
-    def draw(self):
-        pass
-
-img = PgmImage("feep.pgm")
-
-img.draw()
+import os
 
 
 def print_pixel(intensity):
@@ -33,11 +21,52 @@ def print_pixel(intensity):
     print("\33[48;5;" + str(code) + "m  \33[0m", end="")
 
 
+class PgmImage():
+    def __init__(self, file_name):
+        self.__pixels = []
+        self.__width = 0
+        self.__height = 0
+        
+        self.__load_from_file(file_name)
+
+    def __load_from_file(self, file_name):
+        abs_path = os.path.dirname(os.path.realpath(__file__)) + "/" + file_name
+        with open(abs_path, "r") as f:
+            assert f.readline() == "P2\n"
+            
+            # load dimensions
+            self.__width, self.__height = f.readline().split()
+            self.__width = int(self.__width)
+            self.__height = int(self.__height)
+
+            # load color depth
+            white_value = int(f.readline())
+            
+            # reset pixel list
+            self.__pixels = []
+
+            # load all rows
+            for _ in range(self.__height):
+                row = self.__load_row(f, white_value)
+                assert len(row) == self.__width
+                self.__pixels.append(row)
+
+    def __load_row(self, f, white_value):
+        string_pixels = f.readline().split()
+        int_pixels = [int(p) for p in string_pixels]
+        float_pixels = [p / white_value for p in int_pixels]
+        return float_pixels
+
+    def draw(self):
+        for row in self.__pixels:
+            for pixel in row:
+                print_pixel(pixel)
+            print()
+
+
 def main():
-    # dummy: vytiskne spektrum
-    for i in range(20 + 1):
-        print_pixel(i / 20)
-    print()
+    img = PgmImage("feep.pgm")
+    img.draw()
 
 
 main()
